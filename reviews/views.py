ae996934de508e46f81b17a1eb26138226925798
Heriberto Costa Monteiro <heribertoomonteiroo@gmail.com>
2026-03-09 10:12:28 -0300

    feat: Implement pagination for reviews list view and update template

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Review
 
 def list_reviews(request):
     reviews = Review.published.all()
+    paginator = Paginator(reviews, 6)
+    page_number = request.GET.get('page', 1)
+    
+    try:
+        reviews_page = paginator.page(page_number)
+    except(EmptyPage, PageNotAnInteger):
+        reviews_page = paginator.page(1)
 
-    return render(request, 'reviews/list.html', {'reviews': reviews})
+    return render(request, 'reviews/list.html', {'reviews_page': reviews_page})
 
 def review_detail(request, year, month, day, slugfied_title):
     review = get_object_or_404 (
